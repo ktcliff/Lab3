80819f7c74b27f4bae1fbbd7b3a2273782535963
ktcliff <cliffkri@kean.edu>
2023-02-14 17:47:13 -0500

    Commit Test

---
+++ vscode/Chapter04/Lab3/Task5.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab3;
+public class Task5
+{
+
+        /// <summary>
+        /// This method calculates the product of two numbers
+        /// </summary>
+        /// <param name="a">The first number</param>
+        /// <param name="b">The second number</param>
+        /// <returns>The product of the two numbers</returns>
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// This method calculates the product of three numbers
+        /// </summary>
+        /// <param name="a">The first number</param>
+        /// <param name="b">The second number</param>
+        /// <param name="c">The third number</param>
+        /// <returns>The product of the three numbers</returns>
+        public static double Multiply(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+}

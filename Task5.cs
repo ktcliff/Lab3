9f26d45b1525dc58cc075714039bd7f2177da505
ktcliff <cliffkri@kean.edu>
2023-02-15 20:03:22 -0500

    Lab3 commir

---
+++ Task5.cs
@@ -0,0 +1,26 @@
+﻿namespace Task5;
+public class Task5
+{
+        /// <summary>
+        /// This method calculates the product of two numbers
+        /// </summary>
+        /// <param name="d">The first number</param>
+        /// <param name="e">The second number</param>
+        /// <returns>The product of the two numbers</returns>
+        public static double product(double d, double e)
+        {
+            return d * e;
+        }
+
+        /// <summary>
+        /// This method calculates the product of three numbers
+        /// </summary>
+        /// <param name="d">The first number</param>
+        /// <param name="e">The second number</param>
+        /// <param name="f">The third number</param>
+        /// <returns>The product of the three numbers</returns>
+        public static double product(double d, double e, double f)
+        {
+            return d * e * f;
+        }
+}
